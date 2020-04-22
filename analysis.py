5e7be1e0b6ea90e3409280067721b44d794de60f
Andrea Egan <G00388212@gmit.ie>
2020-04-22 23:22:10 +0100

    Created program to output summary to text file

--- analysis.py
+++ analysis.py
@@ -1,19 +1,24 @@
 # Andrea Egan Project 2020 - The Iris Flower Data Set 
-# Objective - output a file as txt ( The Iris data) 
+# Objective - output a summary of each variable to a single txt file
 # analysis this data by using histograms and scatter plots 
 
 import matplotlib.pyplot as plt
 import numpy as np 
 import seaborn as sns 
-import pandas as pd 
+import pandas as pd
+import sys as sy 
 
-# testing the output file.txt
+# testing the output of file.txt
 d = pd. read_csv ("iris.txt") # reads txt file saved within directory. 
-pd.set_option('display.max_rows', 150) # displays all rows when printed 
-pd.set_option('display.max_column', 5) # displays all columns when printed 
+sy.stdout = open("output.txt", "w") # opens and writes to file output.txt
 
-print(d)
-#print (d.shape)
-#print(d.columns)
+pd.set_option('display.max_rows', 10) # displays 10 rows when printed 
+pd.set_option('display.max_column', 5) # displays 5 columns when printed
 
-# *** PLEASE BE AWARE THIS PROGRAM IS STILL BEING BUILT, ADDITIONAL CODE TO BE ADDED***
+# "\n" creates a new line 
+print("SUMMARY OF DATA: ", "\n", d.groupby('species').size(), "\n") 
+print("Count of Columns and Rows: ", d.shape,"\n")
+print("Sample of Data: ", "\n", d.head(10),"\n")  
+print("Description of Data: " "\n", d[d.columns[0:]].describe()) 
+
+sy.stdout.close() # Closing output.txt file )
