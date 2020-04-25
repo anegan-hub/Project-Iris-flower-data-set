b675971647f190613e32df870a51e843b3b663e5
Andrea Egan <G00388212@gmit.ie>
2020-04-25 15:19:31 +0100

    Added list variable name 'col' to create column titles

--- analysis.py
+++ analysis.py
@@ -2,14 +2,12 @@
 # Objective - output a summary of each variable to a single txt file
 # analysis this data by using histograms and scatter plots 
 
-import matplotlib.pyplot as plt
-import numpy as np 
-import seaborn as sns 
 import pandas as pd
-import sys as sy 
+import sys as sy
 
 # testing the output of file.txt
-d = pd. read_csv ("iris.txt") # reads txt file saved within directory. 
+col = ['sepal_length', 'sepal_width','petal_length', 'petal_width','species'] #Creating Column names for dataset 
+d = pd. read_csv ("iris.txt", names=col) # reads txt file within the saved within directory including the column names.
 sy.stdout = open("output.txt", "w") # opens and writes to file output.txt
 
 pd.set_option('display.max_rows', 10) # displays 10 rows when printed 
@@ -19,6 +17,6 @@ pd.set_option('display.max_column', 5) # displays 5 columns when printed
 print("SUMMARY OF DATA: ", "\n", d.groupby('species').size(), "\n") 
 print("Count of Columns and Rows: ", d.shape,"\n")
 print("Sample of Data: ", "\n", d.head(10),"\n")  
-print("Description of Data: " "\n", d[d.columns[0:]].describe()) 
+print("Description of Data: " "\n", d[d.columns[0:]].describe())
 
 sy.stdout.close() # Closing output.txt file )
