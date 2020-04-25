097506b0bff5a86b57e8babcf84f29d9e11aab3e
Andrea Egan <G00388212@gmit.ie>
2020-04-25 23:46:11 +0100

    Adding a program that creates and saves multiple histograms

--- analysis.py
+++ analysis.py
@@ -4,6 +4,9 @@
 
 import pandas as pd
 import sys as sy
+import matplotlib.pyplot as plt
+import numpy as np 
+import seaborn as sns
 
 # testing the output of file.txt
 col = ['sepal_length', 'sepal_width','petal_length', 'petal_width','species'] #Creating Column names for dataset 
@@ -19,4 +22,17 @@ print("Count of Columns and Rows: ", d.shape,"\n")
 print("Sample of Data: ", "\n", d.head(10),"\n")  
 print("Description of Data: " "\n", d[d.columns[0:]].describe())
 
-sy.stdout.close() # Closing output.txt file )
+#sy.stdout.close() # Closing output.txt file )
+
+# Plots
+# Writing a program to create a histogram 
+# of each variable within the iris dataset. 
+# each histogram will be saved automatically when running the program
+
+d.plot.hist( subplots=True, bins=30, layout=(2,2)) #creates multiple histograms
+plt.savefig("Histogram of each variable.png") # saves histogram
+plt.show() 
+
+
+
+
