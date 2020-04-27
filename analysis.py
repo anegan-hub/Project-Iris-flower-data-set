8c27e5d317cf5420566de9e2091f0058a2c7e9a2
Andrea Egan <G00388212@gmit.ie>
2020-04-27 21:46:06 +0100

    adding code for scatterplot

--- analysis.py
+++ analysis.py
@@ -11,7 +11,7 @@ import seaborn as sns
 # testing the output of file.txt
 col = ['sepal_length', 'sepal_width','petal_length', 'petal_width','species'] #Creating Column names for dataset 
 d = pd. read_csv ("iris.txt", names=col) # reads txt file within the saved within directory including the column names.
-sy.stdout = open("output.txt", "w") # opens and writes to file output.txt
+sy.stdout = open("output.txt", "w") # opens file and writes the command print to file output.txt
 
 pd.set_option('display.max_rows', 10) # displays 10 rows when printed 
 pd.set_option('display.max_column', 5) # displays 5 columns when printed
@@ -21,18 +21,27 @@ print("SUMMARY OF DATA: ", "\n", d.groupby('species').size(), "\n")
 print("Count of Columns and Rows: ", d.shape,"\n")
 print("Sample of Data: ", "\n", d.head(10),"\n")  
 print("Description of Data: " "\n", d[d.columns[0:]].describe())
+print(d.info)
 
-#sy.stdout.close() # Closing output.txt file )
+sy.stdout.close() # Closing output.txt file )
 
-# Histogram plot
+# Plots
 # Writing a program to create a histogram 
 # of each variable within the iris dataset. 
 # each histogram will be saved automatically when running the program
 
-d.plot.hist( subplots=True, bins=30, layout=(2,2)) # creating a histogram subplot adjusting bins & layout.
-plt.suptitle("Iris Flower Variables", fontsize = 12) # allows you to add a subtitle to histogram
-plt.tight_layout(rect=[0, 0.03, 1, 0.95]) # prevents data overlapping from png file. 'rect' allows subtitle to fit
+d.plot.hist( subplots=True, bins = 20, layout=(2,2)) # creating a histogram subplot adjusting bins & layout.
+plt.suptitle("Iris Flower Variables", fontsize = 12) # allows you to add a subtitle to histogram,
+plt.tight_layout(rect=[0, 0.03, 1, 0.95]) # prevents layout overlapping from png file. 'rect' allows subtitle to fit within layout
 plt.savefig("Histogram.png")
 plt.show()
 
+# Scattered plots of each pair of variables using seaborn
 
+sns.scatterplot(data = d, x = "sepal_length", y = "sepal_width",  hue="species") 
+plt.title ("Sepal length vs Sepal width")
+plt.show()
+
+sns.scatterplot(data = d, x = "petal_length", y = "petal_width",  hue="species")
+plt.title ("Petal length vs Petal width")
+plt.show()
